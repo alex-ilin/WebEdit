e4bc2d1ac805b5abcd2abd0ad0330fa6fdaf8596
Alexander Ilin <alex.ilin@protonmail.com>
2010-03-30 18:28:35 +0000

    Fixed paths (copy-paste error).
    git-svn-id: svn://agesoft.homeip.net/Amadeus/Npp/WebEdit/trunk@915 8e83561b-3864-4ae1-82b1-2b6e344dab65

--- Src/SetVersion.bat
+++ Src/SetVersion.bat
@@ -8,16 +8,16 @@ if "%1" == "" (
   ) else (
     if "%3" == "" (
       sed -e "3 s/Version .*,/Version %1.%2,/" < Changelog.txt > Changelog.tmp
-      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < ..\WebEditVer > ..\WebEditVer.tmp
-      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < ..\WebEditUVer > ..\WebEditUVer.tmp
+      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < WebEditVer > WebEditVer.tmp
+      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < WebEditUVer > WebEditUVer.tmp
     ) else (
       sed -e "3 s/Version .*,/Version %1.%2.%3,/" < Changelog.txt > Changelog.tmp
-      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < ..\WebEditVer > ..\WebEditVer.tmp
-      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < ..\WebEditUVer > ..\WebEditUVer.tmp
+      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < WebEditVer > WebEditVer.tmp
+      sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < WebEditUVer > WebEditUVer.tmp
     )
-    del Changelog.txt ..\WebEditVer ..\WebEditUVer
+    del Changelog.txt WebEditVer WebEditUVer
     ren Changelog.tmp Changelog.txt
-    ren ..\WebEditVer.tmp WebEditVer
-    ren ..\WebEditUVer.tmp WebEditUVer
+    ren WebEditVer.tmp WebEditVer
+    ren WebEditUVer.tmp WebEditUVer
   )
 )
