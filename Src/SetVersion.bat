8dd376dc2781b0e56570f565c7c97fdc05273b5a
Alexander Ilin <alex.ilin@protonmail.com>
2010-03-31 15:00:25 +0000

    Added: SetVersion.bat now also updates WebEdit.txt.
    git-svn-id: svn://agesoft.homeip.net/Amadeus/Npp/WebEdit/trunk@931 8e83561b-3864-4ae1-82b1-2b6e344dab65

--- Src/SetVersion.bat
+++ Src/SetVersion.bat
@@ -7,15 +7,18 @@ if "%1" == "" (
     echo Error: Ver2 is missing. Run without parameters for help.
   ) else (
     if "%3" == "" (
+      sed -e "1 s/ v[.0-9]* / v%1.%2 /" < WebEdit.txt > WebEdit.tmp
       sed -e "3 s/Version .*,/Version %1.%2,/" < Changelog.txt > Changelog.tmp
       sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < WebEditVer > WebEditVer.tmp
       sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/0/@" < WebEditUVer > WebEditUVer.tmp
     ) else (
+      sed -e "1 s/ v[.0-9]* / v%1.%2.%3 /" < WebEdit.txt > WebEdit.tmp
       sed -e "3 s/Version .*,/Version %1.%2.%3,/" < Changelog.txt > Changelog.tmp
       sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < WebEditVer > WebEditVer.tmp
       sed -e "s@s/$Ver1/.*/@s/$Ver1/%1/@" -e "s@s/$Ver2/.*/@s/$Ver2/%2/@" -e "s@s/$Ver3/.*/@s/$Ver3/%3/@" < WebEditUVer > WebEditUVer.tmp
     )
-    del Changelog.txt WebEditVer WebEditUVer
+    del WebEdit.txt Changelog.txt WebEditVer WebEditUVer
+    ren WebEdit.tmp WebEdit.txt
     ren Changelog.tmp Changelog.txt
     ren WebEditVer.tmp WebEditVer
     ren WebEditUVer.tmp WebEditUVer
